e352a7e7927c013bfd62a3831197dde8e158d21c
Anass N'DRI <anassndri@live.fr>
2024-07-13 15:08:40 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve the static files from the Angular app
+app.use(express.static(path.join(__dirname, 'dist/charles-photos')));
+
+// Redirect all routes to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/charles-photos/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
